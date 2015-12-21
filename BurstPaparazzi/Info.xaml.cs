bc24c15082f6077fc5ee0f0856ee82f836675673
Tyreal Gray <TyrealGray@ymail.com>
2015-12-22 00:01:31 +0800

    update everything api

--- BurstPaparazzi/Info.xaml.cs
+++ BurstPaparazzi/Info.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BurstPaparazzi
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Info.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Info : Window
     {
-
-        public MainWindow()
+        public Info()
         {
             InitializeComponent();
         }
-
     }
 }
