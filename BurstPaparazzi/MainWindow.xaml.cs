e17a85ab63846aadd2e58e2ac5d3973f69b0c1ec
Tyreal Gray <TyrealGray@ymail.com>
2015-12-21 21:06:17 +0800

    change MainWindow style

--- BurstPaparazzi/MainWindow.xaml.cs
+++ BurstPaparazzi/MainWindow.xaml.cs
@@ -13,16 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BurstPaparazzi
-{
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+namespace BurstPaparazzi {
+/// <summary>
+/// MainWindow.xaml 的交互逻辑
+/// </summary>
+public partial class MainWindow : Window {
+
+    public MainWindow() {
+        InitializeComponent();
     }
+
+}
 }
