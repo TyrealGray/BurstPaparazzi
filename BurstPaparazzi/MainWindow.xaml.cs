1ca34cd940286bd1b7b56af1fe480f42a956123c
tyrealgray <tyrealgray@ymail.com>
2016-07-31 12:02:37 +0800

    feat will watch your pc all the time

--- BurstPaparazzi/MainWindow.xaml.cs
+++ BurstPaparazzi/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace BurstPaparazzi
 {
@@ -10,6 +11,8 @@ namespace BurstPaparazzi
     {
         private MainContent m_mainContent = null;
 
+        private NotifyIcon m_notifyIcon;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -17,6 +20,7 @@ namespace BurstPaparazzi
             InitInterface();
 
             BindInterfaceEvent();
+
         }
 
         private void BindInterfaceEvent()
@@ -27,6 +31,31 @@ namespace BurstPaparazzi
         private void InitInterface()
         {
             m_mainContent = new MainContent(mainTabControl);
+
+            this.m_notifyIcon = new NotifyIcon();
+            this.m_notifyIcon.BalloonTipText = "BurstPaparazzi";
+            this.m_notifyIcon.ShowBalloonTip(2000);
+            this.m_notifyIcon.Text = "BurstPaparazzi";
+            this.m_notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
+            this.m_notifyIcon.Visible = true;
+
+            this.m_notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler((o, e) =>
+            {
+                if (e.Button == MouseButtons.Left) this.onShowing(o, e);
+            });
+        }
+
+        private void onShowing(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.Show();
+            this.WindowState = WindowState.Normal;
+        }
+
+        private void onHiding(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.Hide();
         }
 
         private void onClosing(object sender, System.ComponentModel.CancelEventArgs e)
